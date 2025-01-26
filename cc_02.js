abf12033fd5c8167e4559a75b2cf2e55459af392
fabianarequena <fabianarequena@usf.edu>
2025-01-26 14:11:42 -0500

    Task 1 - Store Ineventory

---
+++ cc_02.js
@@ -0,0 +1,5 @@
+// Task 1: Store Ineventory
+let products = ["Outdoor 4k Camera", "Smoke Detector", "PeaGlass Break Detector", "Doorbell 4k Camera", "Sensors"];
+products.push("Panel");
+products.pop();
+console.log("Updated Product List:", products);
